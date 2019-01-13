41006487460384740dcb9e1c32327625d3668116
abigger87@gmail.com <abigger87@gmail.com>
2019-01-13 11:24:08 -0800

    change snapchat button black

--- public/login.js
+++ public/login.js
@@ -20,7 +20,11 @@ window.snapKitInit = function () {
   });
   $(".fp5nm8g").css("height", "50px");
   $(".fp5nm8g").css("background-image", "none");
-  $(".f8ario5").css("padding-left", "0px");
+  $(".fp5nm8g span").css("padding-left", "0px");
+  $(".fp5nm8g").change(function () {
+    $(".f8ario5").css("padding-left", "0px !important");
+  });
+  $(".fp5nm8g").css("color", "black");
 };
 
 // Load SDK asynchronously
